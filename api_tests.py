e3eecc3aa4ef75d68067939ca61818cbb9c319e0
Robert Blake <rblake@mail.umw.edu>
2018-02-19 01:12:02 -0500

    Set up working gmap geocode api.

--- api_tests.py
+++ api_tests.py
@@ -1,11 +1,29 @@
 import os
 from darksky import forecast
+import googlemaps
 
 # Read in token file for DarkSky
 # TODO: add exception if file doesn't exist.
 with open('darksky_token','r') as f: 
 	key = f.read().strip()
 
-boston = forecast(key, 42.3601, -71.0589)
+# Read in token for Google Maps
+with open('gmaps_token','r') as f:
+	gmapsKey = f.read().strip()
 
-print(boston['currently']['temperature'])
+
+# Connect to googlemaps api
+gmaps = googlemaps.Client(gmapsKey)
+
+# Geocoding an address
+geocode_result = gmaps.geocode('1301 College Avenue, Fredericksburg, VA')
+lat = geocode_result[1]['geometry']['location']['lat']
+lng = geocode_result[1]['geometry']['location']['lng']
+ 
+# Fetch conditions at input location
+loc = forecast(key, lat, lng)
+
+
+# Print the current temperature for requested location.
+print(loc['currently']['temperature'])
+
