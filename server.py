179a0664cb27451a6f06a5f09ab214d03dbdd566
Robert Blake <rblake@mail.umw.edu>
2018-03-08 22:09:41 -0500

    Mesh forecast and flask server

--- server.py
+++ server.py
@@ -1,24 +1,37 @@
 import os
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import SocketIO, emit
+from WeatherForecast import WeatherForecast 
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24).encode('hex')
-socketio = SocketIO(app)
+
+wf = WeatherForecast()
 
 # Main Splash
 @app.route('/', methods = ['GET','POST'])
 def home():
-	address = ""
-	date = ""
-	if request.method == 'POST':
-		address = request.form['address']
-		date = request.form['date']
+    address = ""
+    date = ""
+    
+    if request.method == 'POST':
+            address = request.form['address']
+            date = request.form['date']
 
-	print("Here's the input: {0}\t{1}".format(address,date))
+            print("INPUT\nAddress: {0}\nDate: {1}".format(address,date))
 
-	return render_template('index.html',addr=address, time=date)
+            wf.geocode_loc(address)
+            locDat = wf.wForecast()
+
+
+            return render_template('index.html',
+                    addr=address,
+                    time=date,
+                    current=locDat['currently']['temperature'])
+    else:
+        return render_template('index.html')
+
 
 # start the server
 if __name__ == '__main__':
-	app.run(host=os.getenv('IP','0.0.0.0'), port = int(os.getenv('PORT', 8080)), debug=True)
+    app.run(host=os.getenv('IP','0.0.0.0'), port = int(os.getenv('PORT', 8080)), debug=True)
