d6053d06a297a74b73ca201958505e4e83bfad1d
Robert Blake <rblake@mail.umw.edu>
2018-04-06 18:22:22 -0400

    Added 10 year forecast testing script.

---
+++ data_block.py
@@ -0,0 +1,30 @@
+from darksky import forecast
+from datetime import datetime as dt
+
+
+""" Read in your darksky token """
+with open('darksky_token','r') as f: 
+    self.ds_key = f.read().strip()
+
+
+""" These are the coordinates for Fredericksburg, VA """
+lat = 38.3031837
+lng = -77.4605399
+
+
+
+""" year, month, day, hour """
+t = dt(2018, 4, 22, 12).isoformat()
+
+
+# loc = forecast(self.ds_key, self.geoLoc['lat'], self.geoLoc['lng'])
+
+# This is the darksky forecast call, time is optional
+loc = forecast(self.ds_key, self.geoLoc['lat'], self.geoLoc['lng'], time)
+
+# these are some examples of things 
+current = loc['currently']['temperature']
+hourlySum = loc['hourly']['summary']
+high = loc['daily']['data'][0]['temperatureHigh']
+low = loc['daily']['data'][0]['temperatureLow']
+
