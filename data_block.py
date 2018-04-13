d97a890cf1525f49dd62a08082440979d6c4fd5a
Robert Blake <rblake@mail.umw.edu>
2018-04-12 20:12:48 -0400

    Fix test 10 year data pull

--- data_block.py
+++ data_block.py
@@ -1,6 +1,9 @@
 from darksky import forecast
 from datetime import datetime as dt
 
+#####################################
+# --------- SETUP SECTION --------- #
+#####################################
 
 """ Read in your darksky token """
 with open('darksky_token','r') as f: 
@@ -13,35 +16,44 @@ lng = -77.4605399
 
 
 
-""" year, month, day, hour """
-t = dt(2018, 4, 22, 12).isoformat()
+""" Loop through 10 years of the same day 
++ Add it all to list t, of time objects
+"""
+t = []
+for i in range(10):
+    t.append(dt((2000+i), 4, 22, 12).isoformat())
 
-data = 10
-count = 0 
-high = [0] * 10
-low = [0] * 10 
-humidy =[0] * 10 
-dewpoint = [0] * 10 
 
-while count < data: 
-	
-	loc = forecast(ds_key, lat, lng, t)
-	print(count)
-	high[count] = loc['daily']['data'][0]['temperatureHigh']
-	print(t.year)
-	t[0] = t[0] - 1 
-	
-print(high)
+""" Loop through 10 years and use the ds API to request it all
++ Add it all to a list of forecast objects """
+loc = []
+for i in range(10):
+    loc.append(forecast(ds_key, lat, lng, t[i]))
+
+
+# Set it equal to the first entries just to populated them
+high_avg = loc[0]['daily']['data'][0]['temperatureHigh']
+low_avg = loc[0]['daily']['data'][0]['temperatureLow']
+
+""" Loop through all the objects and pull the data we want """
+for i in range(1,10):
+    high_avg += loc[i]['daily']['data'][0]['temperatureHigh']
+    low_avg += loc[i]['daily']['data'][0]['temperatureLow']
+
+high_avg = high_avg/10
+low_avg = low_avg/10
+
+print(high_avg)
+print(low_avg)
 
 # loc = forecast(self.ds_key, self.geoLoc['lat'], self.geoLoc['lng'])
 
 # This is the darksky forecast call, time is optional
-loc = forecast(ds_key, lat, lng, t)
 
 # these are some examples of things 
+"""
 current = loc['currently']['temperature']
 hourlySum = loc['hourly']['summary']
 high = loc['daily']['data'][0]['temperatureHigh']
 low = loc['daily']['data'][0]['temperatureLow']
-
-print(current, hourlySum, high, low)
+"""
